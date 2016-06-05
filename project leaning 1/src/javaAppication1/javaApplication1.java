3a6ac30ac73df0e4aacccd7c0cce9a70c04422b8
Raymundo-Rivera <raymundorivher@gmail.com>
2016-06-05 01:50:53 -0500

    THIS IS A HELLO WORLD

---
+++ project leaning 1/src/javaAppication1/javaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+package javaAppication1;
+
+/**
+ *
+ * @author user
+ */
+public class javaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+    
+}
