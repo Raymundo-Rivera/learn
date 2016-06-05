be0b605a11884f367bf49eecc3c5f545dfd1313b
Raymundo-Rivera <raymundorivher@gmail.com>
2016-06-05 02:16:29 -0500

    testing 2

--- project leaning 1/src/javaAppication1/javaApplication1.java
+++ project leaning 1/src/javaAppication1/javaApplication1.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * test2
+ * test2 again just for learning
  */
  
 package javaAppication1;
